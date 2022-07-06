f082c7d3f79db5b26fc82d35645d6311c2f2d810
my73xmobilcode <my.73x.mobil.code@gmail.com>
2022-07-06 13:15:08 -0700

    completed-assignment01

--- js/script.js
+++ js/script.js
@@ -3,13 +3,132 @@
 //function theMonth();
 //let currentMonth;
 //let summerMonth;
-//let MyLibraryFunction
+//let myLibraryFunction
 
 // STEP 02
-let var1 = 3.9
-let var2 = 'string literal'
-let var3 = false
 
-console.log(var1)
-console.log(var2)
-console.log(var3)
+//let var1 = 3.9                      //num expression          
+//let var2 = 'string literal'         //string expression
+//let var3 = false                    //boolean expression
+//let var4 = null                     // null value
+
+//console.log(var1)
+//console.log(var2)
+//console.log(var3)
+//console.log(var4)
+
+
+// STEP 03
+//let comExpress01 = 1 + (2 * 10) - 3
+//let comExpress02 = (100 / 2) * 100 + 50
+
+//console.log(comExpress01)
+//console.log(comExpress02)
+
+
+// STEP 04
+//Camel notation
+//let firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou
+
+//Hungarian notation
+//let strFirstName, strLastName, strAddress, strCity, strState, iZipCode, iYourAge, strReferralSource, bMayWeContactYou
+
+
+
+// STEP 05
+//let firstName, lastName, address
+//firstName = 'Dave', lastName = 'Eastwood' , address = '123 Main St'
+
+//let firstName = 'Dave', lastName = 'Eastwood' , address = '123 Main St'
+
+//let firstName = 'Dave'
+//let lastName = 'Eastwood'
+//let address = '123 Main St'
+
+// STEP 06
+//let yearsDriving = 10
+//alert(yearsDriving + ' years driving')
+
+// STEP 07
+let varBoStr
+let varNumBol
+
+
+//STEP 08
+//let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//window.console.log(someString);
+
+
+//STEP 09
+//let varNull = null
+//let varUnde = undefined
+//console.log(varNull)
+//console.log(varUnde)
+
+
+//STEP 10
+//let xVar = 10
+//let yVar = +xVar
+//let strVar = '100'
+//let bVarFalse = true
+
+//console.log(yVar)
+//console.log(+strVar)
+//console.log(+bVarFalse)
+
+//STEP 11
+//let myName = 'Abdel Elizarraraz'
+//alert('Hello ' + myName + ' , welcome to the JavaScript class!')
+
+//STEP 12
+//let name = 'Abdel Elizarraraz'
+//alert('Hello ' + myName + ' , welcome to the JavaScript class!')
+
+//STEP 13
+//let name = 'Abdel Elizarraraz'
+//let course = 'JavaScript'
+//alert('Hello ' + name + ' , welcome to the ' + course + ' class!')
+
+
+//STEP 14
+//let name = 'Abdel Elizarraraz'
+//let course = 'JavaScript'
+//alert('Hello ' + name + '.\nwelcome to the ' + course + ' class!')
+
+//STEP 15
+//let course = 'JavaScript'
+//let myName = prompt('Enter your Name: ')
+//alert('Hello ' + myName + '.\nwelcome to the ' + course + ' class!')
+
+//STEP 16
+//let myName = prompt('Enter your Name: ')
+//let myClass = prompt('Enter Class Name: ')
+//alert('Hello ' + myName + '.\nwelcome to the ' + myClass + ' class!')
+
+//STEP 17
+//let varX = 10, varY = 20
+//console.log(varX + varY)
+
+//STEP 18
+//let x = 20;
+//x += 20;
+//console.log(x)
+
+//STEP 19
+//let x = 20;
+//x *= 5;
+//console.log(x)
+
+//STEP 20
+//let x = 20 % 3
+//console.log(x / 1)
+
+//STEP 21
+//let xNum = 100
+//let yNum = '100'
+//console.log('Evaluate =',  xNum == yNum)
+
+//STEP 22
+//let xNum = 100
+//let yNum = '100'
+//console.log('Evaluate =',  xNum === yNum)
